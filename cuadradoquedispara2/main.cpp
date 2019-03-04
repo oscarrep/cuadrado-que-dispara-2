5e178593ac5a2788cdf5adecf833e3d171beaa89
oscarrep <oscar.bcn.95@gmail.com>
2019-03-04 10:37:30 +0100

    square moves arround

--- cuadradoquedispara2/main.cpp
+++ cuadradoquedispara2/main.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "SDL/include/SDL.h"
 
 #pragma comment(lib, "SDL/libx86/SDL2.lib")
@@ -6,5 +7,64 @@
 
 int main(int argc, char* argv[])
 {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	
+	SDL_Event event;
+	
+	bool loop = true;
+	
+	SDL_Window *win = SDL_CreateWindow("RedSquare",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		850,
+		800,
+		SDL_WINDOW_RESIZABLE);
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(win, 0, 0);
+
+	SDL_Rect rect = { 100,100,100,100 };
+	while (loop)
+	{
+		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+		SDL_RenderClear(renderer);
+
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		SDL_RenderFillRect(renderer, &rect);
+		SDL_RenderPresent(renderer);
+
+		if (SDL_PollEvent(&event) != 0) {
+			switch (event.type)
+			{
+			case SDL_KEYDOWN:
+				switch (event.key.keysym.sym) {
+				case SDLK_UP:
+					if (rect.y > 0) {
+						rect.y -= 20;
+					}
+					break;
+				case SDLK_DOWN:
+					if (rect.y < 720 - rect.h) {
+						rect.y += 20;
+					}
+					break;
+				case SDLK_RIGHT:
+					if (rect.x < (720 - rect.w)) {
+						rect.x += 20;
+					}
+					break;
+				case SDLK_LEFT:
+					if (rect.x > 0) {
+						rect.x -= 20;
+					}
+					break;
+				case SDLK_ESCAPE:
+					loop = false;
+					break;
+				}
+			}
+		}
+	}
+
+	SDL_Quit();
 	return 0;
 }
